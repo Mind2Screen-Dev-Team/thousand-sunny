7ea3e484d9184797b8f439a05af26cba19323636
Muhammmad Ali Zufan <muhammadalizufan@gmail.com>
2025-07-16 16:28:12 +0700

    feat: add debug logger for resty

--- app/dependency/dependency.http.client.go
+++ app/dependency/dependency.http.client.go
@@ -1,12 +1,33 @@
 package dependency
 
 import (
+	"context"
+
+	"github.com/Mind2Screen-Dev-Team/thousand-sunny/pkg/xlog"
 	"resty.dev/v3"
 )
 
-func ProvideHttpClient() *resty.Client {
+func ProvideHttpClient(debug *xlog.DebugLogger) *resty.Client {
 	var (
-		r = resty.New()
+		logger = xlog.NewLogger(debug.Logger)
+		client = resty.New().EnableTrace().EnableDebug()
 	)
-	return r
+
+	return client.OnDebugLog(func(dl *resty.DebugLog) {
+		var (
+			fields = []any{
+				"data", dl,
+			}
+			actionMsg = dl.Request.Header.Get("X-Action-Msg")
+			tid       = dl.Request.Header.Get("X-Trace-ID")
+		)
+		if actionMsg == "" {
+			actionMsg = "http resty debug"
+		}
+		if tid != "" {
+			fields = append(fields, "traceId", tid)
+		}
+
+		logger.Info(context.Background(), actionMsg, fields...)
+	})
 }
